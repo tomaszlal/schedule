13508978208db189e3756e9de9b095a115252a9c
Tomasz Lalewicz <lalewicz.tomasz@gmail.com>
2025-10-27 20:46:48 +0100

    added first task

--- src/main/java/tomaszlal/schedule/components/Schedule.java
+++ src/main/java/tomaszlal/schedule/components/Schedule.java
@@ -1,4 +1,15 @@
 package tomaszlal.schedule.components;
 
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalTime;
+
+@Component
 public class Schedule {
+
+    @Scheduled(fixedRate = 10000)
+    public void fixedRateTask() {
+        System.out.println("Run taska on: "+ LocalTime.now());
+    }
 }
