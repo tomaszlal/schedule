9ac360f6be283c1ea2d87779ed385bfabca4f527
tomaszlal <lalewicz.tomasz@gmail.com>
2025-11-10 11:57:56 +0100

    added three cron time

--- src/main/java/tomaszlal/schedule/components/Schedule.java
+++ src/main/java/tomaszlal/schedule/components/Schedule.java
@@ -13,8 +13,18 @@ public class Schedule {
     @Autowired
     private CommandRunner commandRunner;
 
-    @Scheduled(cron = "0 0/3 * * * ?")
-    public void croneTask() throws Exception {
+    @Scheduled(cron = "0 10 23 * * *")
+    public void croneTask0() throws Exception {
+        List<String> commands = Arrays.asList("c:\\script\\ping_run.bat", ">>", "c:\\script\\ping.log");
+        commandRunner.runCommand(commands);
+    }
+    @Scheduled(cron = "0 13 23 * * *")
+    public void croneTask1() throws Exception {
+        List<String> commands = Arrays.asList("c:\\script\\ping_run.bat", ">>", "c:\\script\\ping.log");
+        commandRunner.runCommand(commands);
+    }
+    @Scheduled(cron = "0 17 23 * * *")
+    public void croneTask2() throws Exception {
         List<String> commands = Arrays.asList("c:\\script\\ping_run.bat", ">>", "c:\\script\\ping.log");
         commandRunner.runCommand(commands);
     }
